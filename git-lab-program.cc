630924e73d44a9fc1d7427d2aeb985a8fa46f13d
Cam Zoloty <cz702220@ohio.edu>
2021-09-05 16:39:34 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Cam Zoloty
+ *        Date: 9/5/2021
+ * Description: Gitlab Program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!" << endl;
+    return 0;
+}// main
